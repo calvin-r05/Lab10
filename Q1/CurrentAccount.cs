09bc26433bb2f4adeb729593a4df8c60c3467144
Calvin Regan <S00250043@atu.ie>
2025-01-08 01:00:36 +0000

    Added subclasses of account (savingsaccount and currentaccount) and implemented abstract method into them.

---
+++ Q1/CurrentAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class CurrentAccount : Account
+    {
+        const decimal INTEREST_RATE = 0.03M;
+        public CurrentAccount(string firstName, string lastName, decimal balance, DateTime interestDate)
+            : base (firstName, lastName, balance, interestDate) { }
+
+        public override void CalculateInterest()
+        {
+            DateTime allowedDate = DateTime.Now.AddYears(-1);
+
+            if (allowedDate >= InterestDate)
+            {
+                Balance = Balance + (Balance * INTEREST_RATE);
+                InterestDate = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ Q1/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class SavingsAccount : Account
+    {
+        const decimal INTEREST_RATE = 0.06M;
+        public SavingsAccount(string firstName, string lastName, decimal balance, DateTime interestDate)
+            : base(firstName, lastName, balance, interestDate) { }
+
+        public override void CalculateInterest()
+        {
+            DateTime allowedDate = DateTime.Now.AddYears(-1);
+
+            if (allowedDate >= InterestDate)
+            {
+                Balance = Balance + (Balance * INTEREST_RATE);
+                InterestDate = DateTime.Now;
+            }
+        }
+    }
+}
